fc8a065e5b4d53cd1173d675d1163d3231a9d68d
Mazhar Khan <66962188+khanmazhar@users.noreply.github.com>
2020-08-26 12:07:52 -0700

    update main

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-function rot13(str) {
 
-  var abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"] ;
-  var nop = ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] ;
-  var arr = str.split('');
+document.querySelector('#encode-btn').addEventListener('click', function () {
+    var input = document.querySelector('#encode').value;
 
-  for(var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < abc.length; j++) {
-      if(arr[i] === abc[j]) {
-        arr[i] = nop[j];
-      } else if (arr[i] === nop[j]) {
-        arr[i] = abc[j];
-      }
+    var abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"];
+    var nop = ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var arr = input.toUpperCase().split('');
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < abc.length; j++) {
+            if (arr[i] === abc[j]) {
+                arr[i] = nop[j];
+            } else if (arr[i] === nop[j]) {
+                arr[i] = abc[j];
+            }
+        }
     }
-  }
-  return arr.join('');
-}
 
-rot13("SERR PBQR PNZC");
+    document.querySelector('#encode').value = arr.join('');
+})
+
+document.querySelector('#decode-btn').addEventListener('click', function () {
+    var input = document.querySelector('#decode').value;
+    var abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"];
+    var nop = ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+    var arr = input.toUpperCase().split('');
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < abc.length; j++) {
+            if (arr[i] === abc[j]) {
+                arr[i] = nop[j];
+            } else if (arr[i] === nop[j]) {
+                arr[i] = abc[j];
+            }
+        }
+    }
+    document.querySelector('#decode').value = arr.join('');
+});
